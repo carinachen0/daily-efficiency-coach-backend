bd107f1bcbd7d587c726cf072501e2c47ab8e304
Carina Chen <carina.chen66@myhunter.cuny.edu>
2026-03-30 17:46:27 -0400

    cleaned up code, delete commented block

--- app/models.py
+++ app/models.py
@@ -9,25 +9,6 @@ try:
 except Exception:  # pragma: no cover
     ObjectId = Any
 
-
-# class PyObjectId(ObjectId):
-#     @classmethod
-#     def __get_validators__(cls):
-#         yield cls.validate
-
-#     @classmethod
-#     def validate(cls, v):
-#         if isinstance(v, ObjectId):
-#             return v
-#         if not ObjectId.is_valid(v):
-#             raise ValueError("Invalid ObjectId")
-#         return ObjectId(v)
-
-#     @classmethod
-#     def __get_pydantic_json_schema__(cls, core_schema, handler):
-#         schema = handler(core_schema)
-#         schema.update(type="string")
-#         return schema
 class PyObjectId(ObjectId):
     @classmethod
     def __get_validators__(cls):
